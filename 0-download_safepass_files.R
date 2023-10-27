71a7c217b4eb9a0235f77e57e2f80200d66c53c3
Arthur Cheib <41000381+ArthurCheib@users.noreply.github.com>
2023-10-27 08:43:50 -0500

    Renaming files
    Changing folders

---
+++ 0-download_safepass_files.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(RSocrata)
+library(here)
+
+## Getting the data through the API endpoit
+my_token <- readline(prompt = 'Insert token: ')
+my_email <- readline(prompt = 'Insert email: ')
+my_password <- readline(prompt = 'Insert password: ')
+
+## Queries that will be used
+query <- "select the_geom, schoolid"
+query_2014 <- "select the_geom, school_id"
+
+## Dataset IDs
+dataset_ID <- list(`2013` = 'rq9p-k3zy',
+                   `2014` = 't8dp-yzqg',
+                   `2015` = 'kvm8-tw23',
+                   `2016` = '65ce-agii',
+                   `2017` = 'v3t6-2wdk')
+
+## Function to download all our data
+get_dataset <- function(dic_year_code) {
+  
+  for(year in names(dic_year_code)) {
+    id_temp <- dic_year_code[[year]]
+    
+    # Fetch raw data
+    raw_temp <- read.socrata(paste0("https://data.cityofchicago.org/resource/", id_temp, ".json"),
+                             app_token = my_token,
+                             email = my_email,
+                             password = my_password)
+    
+    # Save the raw data using `here`
+    write_csv(raw_temp,
+              here("0-raw_data", paste0('safepassage_', year, '.csv')))
+  }
+}
+
+# Downloading the datasets
+get_dataset(dic_year_code = dataset_ID)
---
+++ 1-identify_treated_schools.R
@@ -0,0 +1,33 @@
+## Packages
+library(tidyverse)
+library(here)
+
+cps_treated_files <- list.files(here('0-raw_data'), pattern = 'safepassage')
+
+# Initialize an empty dataframe 
+df_final <- data.frame()
+
+for (file in cps_treated_files) {
+  
+  # Read the file
+  df_temp <- read_csv(here('0-raw_data', file))
+  year <- str_extract(file, "\\d{4}")
+  
+  # For 2014, we need to rename the columns to standardize
+  if (year == "2014") {
+    df_temp <- df_temp %>% rename(school_nam = schoolname, schoolid = school_id)
+  }
+  
+  # Extract columns containing 'school'
+  cols <- colnames(df_temp)[str_detect(colnames(df_temp), 'school')]
+  
+  # Create a temporary dataframe with the selected columns and year
+  df_year <- df_temp %>% 
+    select(all_of(cols)) %>% 
+    mutate(year = year)
+  
+  df_final <- bind_rows(df_final, df_year)
+  
+}
+
+write_csv(df_final, here('1-clean_data', 'safe_passage_schools.csv'))
