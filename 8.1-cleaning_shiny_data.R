e25ac5e28b9ab2f883e47fdbebcb6cf1b48c664e
Arthur Cheib <41000381+ArthurCheib@users.noreply.github.com>
2023-10-30 16:43:37 -0500

    Adding shiny
    Shiny + data

--- 8.1-cleaning_shiny_data.R
+++ 8.1-cleaning_shiny_data.R
@@ -35,12 +35,29 @@ write_csv(dataset3, here('clean_data', 'shiny_data.csv'))
 
 
 ## Cleaning the data that will be used for the numeric boxes ##
-schools_db <- read_csv(here('clean_data', 'cps_schools_database.csv'))
+schools_db <- read_csv(here('clean_data', 'cps_schools_database.csv')) %>% 
+  select(school_id, school_lat, school_long)
 crimes_db <- read_csv(here('clean_data', 'crime_data.csv')) %>% 
   select(id, latitude, longitude) %>% 
   setNames(c('crime_id', 'crime_lat', 'crime_long'))
   
-
+## Merging the datasets so as to keep only geolocation relevant information
 data_leaflet <- read_csv(here('clean_data', 'fixed_effects_data.csv')) %>% 
   rename(crime_distance = crime_school_distance) %>% 
-  select(-control)
+  select(-control) %>% 
+  left_join(schools_db, by = 'school_id') %>% 
+  left_join(crimes_db, by = 'crime_id') %>% 
+  mutate(crime_distance = floor(crime_distance * 1.6 * 1000)) %>% 
+  mutate(round_distance = case_when(crime_distance >= 0 & crime_distance <= 25 ~ '25m',
+                                    crime_distance > 25 & crime_distance <= 50 ~ '50m',
+                                    crime_distance > 50 & crime_distance <= 100 ~ '100m',
+                                    crime_distance > 100 & crime_distance <= 200 ~ '200m',
+                                    crime_distance > 200 & crime_distance <= 300 ~ '300m',
+                                    crime_distance > 300 ~ '300m+')) %>% 
+  mutate(round_distance = factor(round_distance, levels = c('25m', '50m', '100m', '200m', '300m', '300m+'))) %>% 
+  select(-c(school_attendance, crime_id, crime_distance)) %>% 
+  select(crime_type, round_distance, everything(.))
+
+# Saving the data
+write_csv(data_leaflet, here('clean_data', 'leaflet_data.csv'))
+
